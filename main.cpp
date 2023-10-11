9626189e3acf7f487b53ea0062f90ec3afe04656
Joshua Brunk <26190850+LunaVoid@users.noreply.github.com>
2023-10-11 16:24:56 -0400

    Origin

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+//Partners: Nitish Goel and Jackie Wang
+//Author: Joshua
+
 #include <iostream>
+#include "StringData.h"
+#include "vector"
+
+int linear_search(vector options, string lookfor){
+
+    std::cout <<a.size()<<std::endl;
+    for(int i = 0; i < a.size()-1; i++){
+        if(lookfor == options[i])
+    }
+}
 
 int main() {
-    std::cout << "Hello, Worlds!" << std::endl;
+    std::vector a = getStringData();
+    linear_search()
     return 0;
+
 }
